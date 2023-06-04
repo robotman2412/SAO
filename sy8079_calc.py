315ffe3f705552943597f06c897b7e093b3ec2d7
RobotMan2412 <julian@scheffers.net>
2023-06-04 11:31:20 +0200

    More works on eprom v2

--- sy8079_calc.py
+++ sy8079_calc.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
-from sys import argv
 import sys
+from sys import argv
 from divider import *
 
-# v = 1.25*(1+x)
-# x = v/1.25 - 1
+# r2 = 0.6 / (v - 0.6) * r1
+# r2 * (v - 0.6) = 0.6 * r1
+# v - 0.6 = 0.6 * r1 / r2
+# v = 0.6 * r1 / r2 + 0.6
+
+# v = 0.6 * x + 0.6
+# 0.6 * x = v - 0.6
+# x = v / 0.6 - 1
 
 if __name__ == "__main__":
 	if len(argv) == 2:
 		voltage   = float(argv[1])
-		ratio     = voltage/1.25 - 1
+		ratio     = voltage/0.6 - 1
 		closest_n = closest_divider(defaults, ratio)
 		print("Ratio for {}V: {}".format(voltage, ratio))
 		print("Closest divider to {}:".format(ratio))
@@ -18,7 +24,7 @@ if __name__ == "__main__":
 	elif len(argv) == 3:
 		voltage   = float(argv[1])
 		n         = int(argv[2])
-		ratio     = voltage/1.25 - 1
+		ratio     = voltage/0.6 - 1
 		closest_n = closest_divider(defaults, ratio, n)
 		print("Ratio for {}V: {}".format(voltage, ratio))
 		if n == 1:
@@ -27,9 +33,9 @@ if __name__ == "__main__":
 			print("Closest {} dividers to {}:".format(n, ratio))
 		
 	else:
-		print("Usage: ap3012_calc.py voltage [n=1]")
+		print("Usage: sy8079_calc.py voltage [n=1]")
 		sys.exit(1)
 	
 	for x, y in closest_n:
-		v = 1.25 + 1.25*x/y
+		v = 0.6 + 0.6*x/y
 		print("  {}/{} yields {}V ({}% error)".format(x, y, v, abs(voltage-v)*100//voltage))
